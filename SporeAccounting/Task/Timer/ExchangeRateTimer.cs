96ffd826c858ea6da47ab0f50b0a4e1ca6aa616c
MiaoShuYo <gangzhucoll1990@live.com>
2024-11-21 16:24:48 +0800

    临时提交代码

---
+++ SporeAccounting/Task/Timer/ExchangeRateTimer.cs
@@ -0,0 +1,13 @@
+using Quartz;
+
+namespace SporeAccounting.Task.Timer;
+/// <summary>
+/// 获取汇率定时器
+/// </summary>
+public class ExchangeRateTimer:IJob
+{
+    public System.Threading.Tasks.Task Execute(IJobExecutionContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
