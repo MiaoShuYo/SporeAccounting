bfbf678ee799d6558231ddf841b5cd49d96aae4c
MiaoShuYo <gangzhucoll1990@live.com>
2025-05-13 01:05:26 +0800

    更新项目结构，删除Common项目，新增SP.Common和SP.IdentityService项目，添加Redis服务及相关配置，更新appsettings.Development.json，调整SP.IdentityService的启动逻辑。

--- SP.IdentityService/Program.cs
+++ SP.IdentityService/Program.cs
@@ -1,36 +1,47 @@
 using Nacos.AspNetCore.V2;
+using SP.Common.Redis;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace SP.IdentityService;
 
-// 添加Nacos配置中心
-builder.Host.ConfigureAppConfiguration((context, builder) =>
+public class Program
 {
-    var configurationRoot = builder.Build();
-    builder.AddNacosV2Configuration(configurationRoot.GetSection("nacos"));
-});
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-// 添加Nacos服务注册
-builder.Services.AddNacosAspNet(builder.Configuration);
+        // 添加Nacos配置中心
+        builder.Host.ConfigureAppConfiguration((_, builder) =>
+        {
+            var configurationRoot = builder.Build();
+            builder.AddNacosV2Configuration(configurationRoot.GetSection("nacos"));
+        });
 
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        // 添加Nacos服务注册
+        builder.Services.AddNacosAspNet(builder.Configuration);
+        // 引入redis
+        builder.Services.AddRedisService(builder.Configuration);
+        builder.Services.AddSingleton<IRedisService, RedisService>();
+        // Add services to the container.
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+        var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
